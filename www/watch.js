e6a8e59ac3b4b38d9722871283bd5d77ca36bd8c
xifei <xifei@nfs.iscas.ac.cn>
2015-07-06 15:09:04 +0800

    fix www/*.js

--- www/watch.js
+++ www/watch.js
@@ -1,17 +1,4 @@
-cordova.define("ibp.plugin.watch.watch", function(require, exports, module) { var Watch = function() {};
-// Watch.prototype.start = function(successCallback, errorCallback) {
-    // cordova.exec(successCallback, errorCallback, "WatchPlugin", "start", []);
-// };
-// Watch.prototype.stop = function(successCallback, errorCallback) {
-    // cordova.exec(successCallback, errorCallback, "WatchPlugin", "stop", []);
-// };
-// Watch.prototype.nextPage = function(successCallback, errorCallback) {
-    // cordova.exec(successCallback, errorCallback, "WatchPlugin", "nextPage", []);
-// };
-// Watch.prototype.prevPage = function(successCallback, errorCallback) {
-    // cordova.exec(successCallback, errorCallback, "WatchPlugin", "prevPage", []);
-// };
-
+var Watch = function() {};
 Watch.prototype.messageToWear = function(successCallback, errorCallback, info){
   cordova.exec(successCallback, errorCallback, "WatchPlugin", "messageToWear", info);
 };
@@ -36,6 +23,4 @@ Watch.prototype.messageFromWear = function(type){
 };
 var WatchObj = new Watch();
 module.exports = WatchObj;
-
-});
 
